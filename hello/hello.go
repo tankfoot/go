77ec2e00684720fcf24e51786bc075631440dda0
Jim <jxia@alumni.scu.edu>
2019-04-16 15:53:45 -0700

    debug

--- hello/hello.go
+++ hello/hello.go
@@ -2,20 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
+	"string"
 )
 
 func main(){
-	//fmt.Println(string.Reverse("hello, world\n"))
-	cmd := exec.Command("gcloud", 
-		"auth",
-		"application-default",
-		"print-access-token",)
-	//var a []string
-	out, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s", out)
+	fmt.Println("Hello !\n")
+	fmt.Println(string.Reverse("hello, world\n"))
 }
