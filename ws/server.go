daedf0bc4278b16293d46d9bb0c14c6ba0c30e86
Jim Xia <jxia@scu.edu>
2019-04-19 18:48:06 -0700

    debug

--- ws/server.go
+++ ws/server.go
@@ -14,10 +14,15 @@ import (
 	"net/http"
     "encoding/json"
     "os/exec"
+    "errors"
+    "io/ioutil"
+    "bytes"
 
 	"github.com/gorilla/websocket"
+    sj "github.com/bitly/go-simplejson"
 )
 
+//Incoming Json struct
 type Data struct {
     Query string
 }
@@ -27,16 +32,57 @@ type Message struct {
     Data   Data
 }
 
+//Dialogflow Query struct
+type Text struct {
+        Text string `json:"text"`
+        LanguageCode string `json:"languageCode"`
+}
+
+type TextInput struct {
+        TextInput Text `json:"text"`
+}
+
+type QueryInput struct {
+    QueryInput TextInput `json:"queryInput"`
+}
 
 //var addr = flag.String("addr", "localhost:8080", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
 
-// func DetectIntentText(projectID, sessionID, text, languageCode string) (string, error) {
-//     if projectID == "" || sessionID == "" {
-//         return "", errors.New(fmt.Sprintf("Received empty project (%s) or session (%s)", projectID, sessionID))
-//     }
-// }
+func DetectIntentText(projectID, sessionID, text, languageCode string) (string, error) {
+    if projectID == "" || sessionID == "" {
+        return "", errors.New(fmt.Sprintf("Received empty project (%s) or session (%s)", projectID, sessionID))
+    }
+    basePath := "https://dialogflow.googleapis.com/v2/"
+    sessionPath := fmt.Sprintf("projects/%s/agent/sessions/%s", projectID, sessionID)
+
+    client := &http.Client{}
+    var jsonData QueryInput
+    jsonData.QueryInput = TextInput{Text{Text: text, LanguageCode: languageCode}}
+    jsonValue, _ := json.Marshal(jsonData)
+    detectIntentUrl := basePath + sessionPath + ":detectIntent"
+    r, err := http.NewRequest("POST", detectIntentUrl, bytes.NewBuffer(jsonValue))
+
+    token, _ := GetGcloudToken()
+    var bearer = "Bearer " + token
+    r.Header.Add("Authorization", bearer)
+    r.Header.Add("Content-Type", "application/json; charset=utf-8")
+
+    resp, err := client.Do(r)
+
+    if err != nil {
+        fmt.Printf("The HTTP request failed with error %s\n", err)
+    } else {
+        data, _ := ioutil.ReadAll(resp.Body)
+        js, _ := sj.NewJson(data)
+        r := js.Get("queryResult").Get("fulfillmentText").MustString()
+        fmt.Printf("%s\n", r)
+        return r, nil
+    }
+
+    return "", nil
+}
 
 func GetGcloudToken() (string, error) {
     cmd := exec.Command("gcloud", 
@@ -74,9 +120,9 @@ func echo(w http.ResponseWriter, r *http.Request) {
         if err1 != nil {
             log.Fatalln("error:", err1)
         }
-		err = c.WriteMessage(mt, []byte(m.Data.Query))
-        token, _ := GetGcloudToken()
-        fmt.Printf(token)
+        rr, _ := DetectIntentText("chipotle-aeeb4", "123", m.Data.Query, "en")
+		err = c.WriteMessage(mt, []byte(rr))
+
 		if err != nil {
 			log.Println("write:", err)
 			break
